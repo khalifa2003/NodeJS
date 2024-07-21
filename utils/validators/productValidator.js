8f67f576a128960a3e02fdc757b7c3e346a3ba7b
Ahmad Khalifa AbdUlraouf <132180439+khalifa2003@users.noreply.github.com>
2024-07-21 22:33:20 +0300

    edit schemas

--- utils/validators/productValidator.js
+++ utils/validators/productValidator.js
@@ -1,60 +1,49 @@
-const slugify = require('slugify');
-const { check, body } = require('express-validator');
-const validatorMiddleware = require('../../middlewares/validatorMiddleware');
-const Category = require('../../models/categoryModel');
-const SubCategory = require('../../models/subCategoryModel');
+const slugify = require("slugify");
+const { check, body } = require("express-validator");
+const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const Category = require("../../models/categoryModel");
+const SubCategory = require("../../models/subCategoryModel");
 
 exports.createProductValidator = [
-  check('title')
+  check("title")
     .isLength({ min: 3 })
-    .withMessage('must be at least 3 chars')
+    .withMessage("must be at least 3 chars")
     .notEmpty()
-    .withMessage('Product Title required')
+    .withMessage("Product Title required")
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
       return true;
     }),
-  check('description')
+  check("description")
     .notEmpty()
-    .withMessage('Product description is required')
+    .withMessage("Product description is required")
     .isLength({ min: 50 })
-    .withMessage('Too Short description'),
-  check('quantity')
+    .withMessage("Too Short description"),
+  check("quantity")
     .notEmpty()
-    .withMessage('Product quantity is required')
+    .withMessage("Product quantity is required")
     .isNumeric()
-    .withMessage('Product quantity must be a number'),
-  check('sold')
+    .withMessage("Product quantity must be a number"),
+  check("sold")
     .optional()
     .isNumeric()
-    .withMessage('Product quantity must be a number'),
-  check('price')
+    .withMessage("Product quantity must be a number"),
+  check("price")
     .notEmpty()
-    .withMessage('Product price is required')
+    .withMessage("Product price is required")
     .isNumeric()
-    .withMessage('Product price must be a number')
+    .withMessage("Product price must be a number")
     .isLength({ max: 32 })
-    .withMessage('To long price'),
-  check('priceAfterDiscount')
-    .optional()
-    .isNumeric()
-    .withMessage('Product priceAfterDiscount must be a number')
-    .toFloat()
-    .custom((value, { req }) => {
-      if (req.body.price <= value) {
-        throw new Error('priceAfterDiscount must be lower than price');
-      }
-      return true;
-    }),
-  check('image')
+    .withMessage("To long price"),
+  check("images")
     .optional()
     .isArray()
-    .withMessage('images should be array of string'),
-  check('category')
+    .withMessage("images should be array of string"),
+  check("category")
     .notEmpty()
-    .withMessage('Product must be belong to a category')
+    .withMessage("Product must be belong to a category")
     .isMongoId()
-    .withMessage('Invalid ID formate')
+    .withMessage("Invalid ID formate")
     .custom((categoryId) =>
       Category.findById(categoryId).then((category) => {
         if (!category) {
@@ -65,10 +54,10 @@ exports.createProductValidator = [
       })
     ),
 
-  check('subcategories')
+  check("subcategories")
     .optional()
     .isMongoId()
-    .withMessage('Invalid ID formate')
+    .withMessage("Invalid ID formate")
     .custom((subcategoriesIds) =>
       SubCategory.find({ _id: { $exists: true, $in: subcategoriesIds } }).then(
         (result) => {
@@ -96,31 +85,31 @@ exports.createProductValidator = [
       )
     ),
 
-  check('brand').optional().isMongoId().withMessage('Invalid ID formate'),
-  check('ratingsAverage')
+  check("brand").optional().isMongoId().withMessage("Invalid ID formate"),
+  check("ratingsAverage")
     .optional()
     .isNumeric()
-    .withMessage('ratingsAverage must be a number')
+    .withMessage("ratingsAverage must be a number")
     .isLength({ min: 1 })
-    .withMessage('Rating must be above or equal 1.0')
+    .withMessage("Rating must be above or equal 1.0")
     .isLength({ max: 5 })
-    .withMessage('Rating must be below or equal 5.0'),
-  check('ratingsQuantity')
+    .withMessage("Rating must be below or equal 5.0"),
+  check("ratingsQuantity")
     .optional()
     .isNumeric()
-    .withMessage('ratingsQuantity must be a number'),
+    .withMessage("ratingsQuantity must be a number"),
 
   validatorMiddleware,
 ];
 
 exports.getProductValidator = [
-  check('id').isMongoId().withMessage('Invalid ID formate'),
+  check("id").isMongoId().withMessage("Invalid ID formate"),
   validatorMiddleware,
 ];
 
 exports.updateProductValidator = [
-  check('id').isMongoId().withMessage('Invalid ID formate'),
-  body('title')
+  check("id").isMongoId().withMessage("Invalid ID formate"),
+  body("title")
     .optional()
     .custom((val, { req }) => {
       req.body.slug = slugify(val);
@@ -130,6 +119,6 @@ exports.updateProductValidator = [
 ];
 
 exports.deleteProductValidator = [
-  check('id').isMongoId().withMessage('Invalid ID formate'),
+  check("id").isMongoId().withMessage("Invalid ID formate"),
   validatorMiddleware,
 ];
